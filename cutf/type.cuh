d81b98788a6356de195e86c495441af29fbded99
mutsuki <mutsuki@momo86.net>
2018-11-02 09:03:59 +0900

    update : add reinterpret

--- cutf/type.cuh
+++ cutf/type.cuh
@@ -5,6 +5,8 @@
 
 #define CAST(from_t, to_t, func, val) \
 	template <> __host__ __device__ to_t cast<to_t>(const from_t val){return func;}
+#define REINTERPRET(src_type, src_ty, dst_type, dst_ty) \
+	template <> __device__ dst_type reinterpret<dst_type>(const src_type a){return __##src_ty##_as_##dst_ty(a);}
 
 namespace{
 namespace cuda {
@@ -24,6 +26,19 @@ CAST(float, double, static_cast<double>(a), a);
 CAST(double, half, __half2float(static_cast<float>(a)), a);
 CAST(double, float, static_cast<float>(a), a);
 CAST(double, double, a, a);
+
+// reinterpret
+template <class T> __device__ T reinterpret(const float a);
+template <class T> __device__ T reinterpret(const double a);
+template <class T> __device__ T reinterpret(const long long a);
+template <class T> __device__ T reinterpret(const unsigned int a);
+template <class T> __device__ T reinterpret(const int a);
+REINTERPRET(float, float, unsigned int, uint);
+REINTERPRET(float, float, int, int);
+REINTERPRET(double, double, long long, longlong);
+REINTERPRET(int, int, float, float);
+REINTERPRET(unsigned int, uint, float, float);
+REINTERPRET(long long, longlong, double, double);
 
 } // namespace type	
 } // cuda
