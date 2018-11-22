41194efe63bc74ac738a6ac5145d133cd5e860cc
mutsuki <mutsuki@momo86.net>
2018-11-22 21:48:13 +0900

    add device.hpp

---
+++ cutf/device.hpp
@@ -0,0 +1,23 @@
+#ifndef __CUTF_DEVICE_HPP__
+#define __CUTF_DEVICE_HPP__
+
+#include <vector>
+
+namespace cutf{
+namespace device{
+inline std::vector<cudaDeviceProp> get_properies_vector(){
+	int n;
+	cudaGetDeviceCount(&n);
+	std::vector<cudaDeviceProp> properties_vector;
+	for(int i = 0; i < n; i++){
+		cudaDeviceProp property;
+		cudaGetDeviceProperties(&property, i);
+		properties_vector.push_back(property);
+	}
+
+	return properties_vector;
+}
+} // device
+} // cutf
+
+#endif // __CUTF_DEVICE_HPP__
