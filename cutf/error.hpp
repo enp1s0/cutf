f25f2bdad095d165cd67e37c0e76b815a592b5d9
mutsuki <mutsuki@momo86.net>
2018-11-23 12:25:03 +0900

    update : add cublas error check

--- cutf/error.hpp
+++ cutf/error.hpp
@@ -2,17 +2,44 @@
 #define __CUTF_ERROR_CUH__
 #include <stdexcept>
 #include <sstream>
+#include <cublas_v2.h>
 
 namespace cutf{
 namespace cuda{
 namespace error{
-inline void check(cudaError_t error, const std::string filename, const std::size_t line, const std::string funcname){
+inline void check(cudaError_t error, const std::string filename = __FILE__, const std::size_t line = __LINE__, const std::string funcname = __func__){
 	if(error != cudaSuccess){
 		std::stringstream ss;
 		ss<< cudaGetErrorString( error ) <<" ["<<filename<<":"<<line<<" in "<<funcname<<"]";
 		throw std::runtime_error(ss.str());
 	}
 }
+
+} // error
+} // cuda
+namespace cublas{
+namespace error{
+inline void check(cublasStatus_t error, const std::string filename = __FILE__, const std::size_t line = __LINE__, const std::string funcname = __func__){
+	if(error != CUBLAS_STATUS_SUCCESS){
+		std::string error_string;
+#define CUBLAS_ERROR_CASE(c) case c: error_string = #c; break
+		switch(error){
+			CUBLAS_ERROR_CASE( CUBLAS_STATUS_SUCCESS );
+			CUBLAS_ERROR_CASE( CUBLAS_STATUS_NOT_INITIALIZED );
+			CUBLAS_ERROR_CASE( CUBLAS_STATUS_ALLOC_FAILED );
+			CUBLAS_ERROR_CASE( CUBLAS_STATUS_INVALID_VALUE );
+			CUBLAS_ERROR_CASE( CUBLAS_STATUS_ARCH_MISMATCH );
+			CUBLAS_ERROR_CASE( CUBLAS_STATUS_MAPPING_ERROR );
+			CUBLAS_ERROR_CASE( CUBLAS_STATUS_EXECUTION_FAILED );
+			CUBLAS_ERROR_CASE( CUBLAS_STATUS_INTERNAL_ERROR );
+		default: error_string = "Unknown error"; break;
+		}
+		std::stringstream ss;
+		ss<< error_string <<" ["<<filename<<":"<<line<<" in "<<funcname<<"]";
+		throw std::runtime_error(ss.str());
+	}
+}
+
 } // error
 } // cuda
 } // cutf
