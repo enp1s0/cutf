16d0624ec9732c14b9ec624668038e6edae7bca0
mutsuki <mutsuki@momo86.net>
2018-11-30 09:52:43 +0900

    update : move cublas::error::check from error.hpp to cublas.hpp

--- cutf/cublas.hpp
+++ cutf/cublas.hpp
@@ -5,6 +5,29 @@
 
 namespace cutf{
 namespace cublas{
+namespace error{
+inline void check(cublasStatus_t error, const std::string filename, const std::size_t line, const std::string funcname){
+	if(error != CUBLAS_STATUS_SUCCESS){
+		std::string error_string;
+#define CUBLAS_ERROR_CASE(c) case c: error_string = #c; break
+		switch(error){
+			CUBLAS_ERROR_CASE( CUBLAS_STATUS_SUCCESS );
+			CUBLAS_ERROR_CASE( CUBLAS_STATUS_NOT_INITIALIZED );
+			CUBLAS_ERROR_CASE( CUBLAS_STATUS_ALLOC_FAILED );
+			CUBLAS_ERROR_CASE( CUBLAS_STATUS_INVALID_VALUE );
+			CUBLAS_ERROR_CASE( CUBLAS_STATUS_ARCH_MISMATCH );
+			CUBLAS_ERROR_CASE( CUBLAS_STATUS_MAPPING_ERROR );
+			CUBLAS_ERROR_CASE( CUBLAS_STATUS_EXECUTION_FAILED );
+			CUBLAS_ERROR_CASE( CUBLAS_STATUS_INTERNAL_ERROR );
+		default: error_string = "Unknown error"; break;
+		}
+		std::stringstream ss;
+		ss<< error_string <<" ["<<filename<<":"<<line<<" in "<<funcname<<"]";
+		throw std::runtime_error(ss.str());
+	}
+}
+
+} // error
 // ==================================================
 // BLAS Lv 1
 // ==================================================
