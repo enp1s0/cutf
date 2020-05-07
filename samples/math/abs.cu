3ea13495316a994d026b7b26a0321d31717b978d
mutsuki <mutsuki@momo86.net>
2020-05-07 21:44:20 +0900

    Add half2 test

--- samples/math/abs.cu
+++ samples/math/abs.cu
@@ -37,10 +37,38 @@ void test_abs(const std::size_t N){
 	}
 	std::cout<<std::endl;
 }
+
+template <>
+void test_abs<half2>(const std::size_t N){
+	std::cout<<"# "<<cutf::type::get_type_name<half2>()<<" test --"<<std::endl;
+	auto dM = cutf::memory::get_device_unique_ptr<half2>(N);
+	auto hM = cutf::memory::get_host_unique_ptr<half2>(N);
+	std::cout<<"m = ";
+	for(auto i = decltype(N)(0); i < N; i++){
+		hM.get()[i].x = cutf::type::cast<half>(N/2.0f - i);
+		hM.get()[i].y = cutf::type::cast<half>(-(N/2.0f - i));
+		std::cout<<cutf::type::cast<float>(hM.get()[i].x)<<" ";
+		std::cout<<cutf::type::cast<float>(hM.get()[i].y)<<" ";
+	}
+	std::cout<<std::endl;
+	cutf::memory::copy(dM.get(), hM.get(), N);
+
+	abs_kernel<half2><<<(N + threads_per_block - 1)/threads_per_block, threads_per_block>>>(dM.get(), N);
+
+	cutf::memory::copy(hM.get(), dM.get(), N);
+
+	std::cout<<"|m| = ";
+	for(auto i = decltype(N)(0); i < N; i++){
+		std::cout<<cutf::type::cast<float>(hM.get()[i].x)<<" ";
+		std::cout<<cutf::type::cast<float>(hM.get()[i].y)<<" ";
+	}
+	std::cout<<std::endl;
+}
 }
 
 int main(){
 	test_abs<half>(N);
+	test_abs<half2>(N);
 	test_abs<float>(N);
 	test_abs<double>(N);
 }
