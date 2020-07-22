1dd4a757bcaa8ef1c3a58974365f3961392dd68f
mutsuki <mutsuki@momo86.net>
2020-07-22 19:00:03 +0900

    Add `debug::tf32::detail` namespace

--- cutf/debug/tf32.hpp
+++ cutf/debug/tf32.hpp
@@ -5,15 +5,21 @@
 namespace cutf {
 namespace debug {
 namespace tf32 {
-union uint32_bitstring_convert {
+namespace detail {
+union to_bitstring {
 	float fp;
 	uint32_t bitstring;
 };
+union to_fp {
+	uint32_t bitstring;
+	float fp;
+};
+} // namespace detail
 
 using tf32_t = float;
 
 __device__ __host__ inline cutf::debug::tf32::tf32_t to_tf32(const float v) {
-	const uint32_t in = uint32_bitstring_convert{v}.bitstring;
+	const uint32_t in = detail::to_bitstring{v}.bitstring;
 	const uint32_t c0 = (in & 0b0'00000000'0000000000'1000000000000u);
 	const uint32_t m = (in & 0b0'00000000'1111111111'0000000000000u);
 	const uint32_t e = (in & 0b0'11111111'0000000000'0000000000000u);
@@ -25,7 +31,7 @@ __device__ __host__ inline cutf::debug::tf32::tf32_t to_tf32(const float v) {
 	const uint32_t e_pre = e + (c1 << 23);
 
 	const uint32_t out = s | m_pre | e_pre;
-	return *reinterpret_cast<const float*>(&out);
+	return detail::to_fp{out}.fp;
 }
 
 } // namespace tf32
