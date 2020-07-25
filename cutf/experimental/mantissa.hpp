7184ad7c3ffbbc385199c9c00708b464abf848cd
mutsuki <mutsuki@momo86.net>
2020-07-25 23:15:54 +0900

    Add `cut_mantissa` for FP64

--- cutf/experimental/mantissa.hpp
+++ cutf/experimental/mantissa.hpp
@@ -43,6 +43,27 @@ __device__ __host__ inline float cut_mantissa(const float v) {
 	const uint32_t out = s | m_pre | e_pre;
 	return cutf::experimental::detail::bitstring_to_fp32{out}.fp;
 }
+
+template <unsigned mantissa_length>
+__device__ __host__ inline double cut_mantissa(const double v) {
+	static_assert(mantissa_length > 0, "mantissa_length must be greater than 0");
+	static_assert(mantissa_length < 52, "mantissa_length must be smaller than 52");
+
+	constexpr unsigned cut_length = 52u - mantissa_length;
+	const uint64_t in = cutf::experimental::detail::fp64_to_bitstring{v}.bitstring;
+	const uint64_t c0 = (in & (1u << (cut_length - 1)));
+	const uint64_t m = (in & (0x000ffffffffffffflu - ((1llu << cut_length) - 1)));
+	const uint64_t e = (in & 0xfff0000000000000lu);
+	const uint64_t s = (in & 0x8000000000000000lu);
+
+	const uint64_t m0 = m + (c0 << 1);
+	const uint64_t c1 = (m0 & 0x0010000000000000lu) >> 52;
+	const uint64_t m_pre = m0 & (0x000ffffffffffffflu - ((1lu << cut_length) - 1));
+	const uint64_t e_pre = e + (c1 << 52);
+
+	const uint64_t out = s | m_pre | e_pre;
+	return cutf::experimental::detail::bitstring_to_fp64{out}.fp;
+}
 } // namespace experimental
 } // namespace cutf
 #endif
