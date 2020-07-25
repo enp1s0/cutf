e54d6e07af86c9c089c657070497a87e5e7c38f7
mutsuki <mutsuki@momo86.net>
2020-07-25 22:33:41 +0900

    Create mantissa.hpp

---
+++ cutf/experimental/mantissa.hpp
@@ -0,0 +1,9 @@
+#ifndef __CUTF_EXPERIMENTAL_MANTISSA_HPP__
+#define __CUTF_EXPERIMENTAL_MANTISSA_HPP__
+#include <cstdint>
+namespace cutf {
+namespace experimental {
+
+} // namespace experimental
+} // namespace cutf
+#endif
