1c7dff5ebed97ae4dcd0e80affc61caca2a2691e
mutsuki <mutsuki@momo86.net>
2020-07-25 22:40:41 +0900

    Update to use bitstring<>fp32 converting function in mantissa.hpp

--- cutf/experimental/tf32.hpp
+++ cutf/experimental/tf32.hpp
@@ -1,25 +1,16 @@
 #ifndef __CUTF_DEBUG_TF32_HPP__
 #define __CUTF_DEBUG_TF32_HPP__
 #include <cinttypes>
+#include "mantissa.hpp"
 
 namespace cutf {
 namespace experimental {
 namespace tf32 {
-namespace detail {
-union to_bitstring {
-	float fp;
-	uint32_t bitstring;
-};
-union to_fp {
-	uint32_t bitstring;
-	float fp;
-};
-} // namespace detail
 
 using tf32_t = float;
 
 __device__ __host__ inline cutf::experimental::tf32::tf32_t to_tf32(const float v) {
-	const uint32_t in = detail::to_bitstring{v}.bitstring;
+	const uint32_t in = cutf::experimental::detail::fp32_to_bitstring{v}.bitstring;
 	const uint32_t c0 = (in & 0b0'00000000'0000000000'1000000000000u);
 	const uint32_t m = (in & 0b0'00000000'1111111111'0000000000000u);
 	const uint32_t e = (in & 0b0'11111111'0000000000'0000000000000u);
@@ -31,7 +22,7 @@ __device__ __host__ inline cutf::experimental::tf32::tf32_t to_tf32(const float 
 	const uint32_t e_pre = e + (c1 << 23);
 
 	const uint32_t out = s | m_pre | e_pre;
-	return detail::to_fp{out}.fp;
+	return cutf::experimental::detail::bitstring_to_fp32{out}.fp;
 }
 
 } // namespace tf32
