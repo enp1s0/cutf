dc10fab4be914dd41acad72286ec0fc7a98797c9
mutsuki <mutsuki@momo86.net>
2020-07-25 22:48:54 +0900

    Update to use `cutf::experimental::cut_mantissa<10>`

--- cutf/experimental/tf32.hpp
+++ cutf/experimental/tf32.hpp
@@ -10,19 +10,7 @@ namespace tf32 {
 using tf32_t = float;
 
 __device__ __host__ inline cutf::experimental::tf32::tf32_t to_tf32(const float v) {
-	const uint32_t in = cutf::experimental::detail::fp32_to_bitstring{v}.bitstring;
-	const uint32_t c0 = (in & 0b0'00000000'0000000000'1000000000000u);
-	const uint32_t m = (in & 0b0'00000000'1111111111'0000000000000u);
-	const uint32_t e = (in & 0b0'11111111'0000000000'0000000000000u);
-	const uint32_t s = (in & 0b1'00000000'0000000000'0000000000000u);
-
-	const uint32_t m0 = m + (c0 << 1);
-	const uint32_t c1 = (m0 & 0b0'00000001'0000000000'0000000000000u) >> 23;
-	const uint32_t m_pre = m0 & 0b0'00000000'1111111111'0000000000000u;
-	const uint32_t e_pre = e + (c1 << 23);
-
-	const uint32_t out = s | m_pre | e_pre;
-	return cutf::experimental::detail::bitstring_to_fp32{out}.fp;
+	return cutf::experimental::cut_mantissa<10>(v);
 }
 
 } // namespace tf32
