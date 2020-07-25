3172eb1dc7eb2f2fcf9a1c721e777b369dd7d675
mutsuki <mutsuki@momo86.net>
2020-07-25 22:41:14 +0900

    Add `fp32_to_bitstring` and `bitstring_to_fp32`

--- cutf/experimental/mantissa.hpp
+++ cutf/experimental/mantissa.hpp
@@ -1,9 +1,19 @@
 #ifndef __CUTF_EXPERIMENTAL_MANTISSA_HPP__
 #define __CUTF_EXPERIMENTAL_MANTISSA_HPP__
-#include <cstdint>
+#include <cinttypes>
+
 namespace cutf {
 namespace experimental {
-
+namespace detail {
+union fp32_to_bitstring {
+	float fp;
+	uint32_t bitstring;
+};
+union bitstring_to_fp32 {
+	uint32_t bitstring;
+	float fp;
+};
+} // namespace detail
 } // namespace experimental
 } // namespace cutf
 #endif
