b2a7e5703efc31e2023ea7e03031552ba979c4a4
mutsuki <mutsuki@momo86.net>
2020-08-07 14:55:10 +0900

    Add comment

--- cutf/macro.hpp
+++ cutf/macro.hpp
@@ -1,5 +1,6 @@
 #ifndef __CUTF_MACRO_HPP__
 #define __CUTF_MACRO_HPP__
+
 #if !defined(CUTF_DEVICE_HOST_FUNC) && defined(__CUDA_ARCH__)
 #define CUTF_DEVICE_HOST_FUNC __device__ __host__
 #elif !defined(CUTF_DEVICE_HOST_FUNC)
@@ -12,4 +13,4 @@
 #define CUTF_DEVICE_FUNC
 #endif
 
-#endif
+#endif // __CUTF_MACRO_HPP__
