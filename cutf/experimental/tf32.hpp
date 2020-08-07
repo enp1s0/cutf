acbde029c7a4c199f1d7256ddcda1f2e4b06708f
mutsuki <mutsuki@momo86.net>
2020-08-07 11:37:00 +0900

    Update to use `CUTF_DEVICE_HOST` in tf32.hpp

--- cutf/experimental/tf32.hpp
+++ cutf/experimental/tf32.hpp
@@ -3,13 +3,19 @@
 #include <cinttypes>
 #include "mantissa.hpp"
 
+#if !defined(CUTF_DEVICE_HOST) && defined(__CUDA_ARCH__)
+#define CUTF_DEVICE_HOST CUTF_DEVICE_HOST
+#else
+#define CUTF_DEVICE_HOST
+#endif
+
 namespace cutf {
 namespace experimental {
 namespace tf32 {
 
 using tf32_t = float;
 
-__device__ __host__ inline cutf::experimental::tf32::tf32_t to_tf32(const float v) {
+CUTF_DEVICE_HOST inline cutf::experimental::tf32::tf32_t to_tf32(const float v) {
 	return cutf::experimental::cut_mantissa<10>(v);
 }
 
