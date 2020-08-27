378f82d4aa236d2699bd0a63ac269b4d6d768988
mutsuki <mutsuki@momo86.net>
2020-08-27 22:15:27 +0900

    Add experimental/exponent.hpp

---
+++ cutf/experimental/exponent.hpp
@@ -0,0 +1,9 @@
+#ifndef __CUTF_EXPERIMENTAL_EXPONENT_HPP__
+#define __CUTF_EXPERIMENTAL_EXPONENT_HPP__
+#include <cstdint>
+
+namespace cutf {
+namespace experimental {
+} // namespace experimental
+} // namespace cutf
+#endif
