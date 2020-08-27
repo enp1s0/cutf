efb2cff9efd807bb51589b5284c7a01cb7d100d8
mutsuki <mutsuki@momo86.net>
2020-08-27 22:26:54 +0900

    Add the definition of `get_mantissa_size`

--- cutf/experimental/fp.hpp
+++ cutf/experimental/fp.hpp
@@ -11,6 +11,12 @@ CUTF_DEVICE_HOST_FUNC inline unsigned get_exponent_size();
 template <> CUTF_DEVICE_HOST_FUNC inline unsigned get_exponent_size<half  >() {return 5;}
 template <> CUTF_DEVICE_HOST_FUNC inline unsigned get_exponent_size<float >() {return 8;}
 template <> CUTF_DEVICE_HOST_FUNC inline unsigned get_exponent_size<double>() {return 11;}
+
+template <class T>
+CUTF_DEVICE_HOST_FUNC inline unsigned get_mantissa_size();
+template <> CUTF_DEVICE_HOST_FUNC inline unsigned get_mantissa_size<half  >() {return 10;}
+template <> CUTF_DEVICE_HOST_FUNC inline unsigned get_mantissa_size<float >() {return 23;}
+template <> CUTF_DEVICE_HOST_FUNC inline unsigned get_mantissa_size<double>() {return 52;}
 } // namespace fp
 } // namespace experimental
 } // namespace cutf
