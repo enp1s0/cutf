b5004b3de698448ec796a5fcc8380e916ff97c85
mutsuki <mutsuki@momo86.net>
2020-08-27 22:25:53 +0900

    Add the definition of `get_exponent_size`

--- cutf/experimental/fp.hpp
+++ cutf/experimental/fp.hpp
@@ -1,10 +1,17 @@
 #ifndef __CUTF_EXPERIMENTAL_FP_HPP__
 #define __CUTF_EXPERIMENTAL_FP_HPP__
 #include "../macro.hpp"
+#include <cuda_fp16.h>
 
 namespace cutf {
 namespace experimental {
-
+namespace fp {
+template <class T>
+CUTF_DEVICE_HOST_FUNC inline unsigned get_exponent_size();
+template <> CUTF_DEVICE_HOST_FUNC inline unsigned get_exponent_size<half  >() {return 5;}
+template <> CUTF_DEVICE_HOST_FUNC inline unsigned get_exponent_size<float >() {return 8;}
+template <> CUTF_DEVICE_HOST_FUNC inline unsigned get_exponent_size<double>() {return 11;}
+} // namespace fp
 } // namespace experimental
 } // namespace cutf
 #endif
