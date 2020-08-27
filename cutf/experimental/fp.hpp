5612723fe1605ffa446d2d8502cd8b26e4cf2b22
mutsuki <mutsuki@momo86.net>
2020-08-27 22:31:45 +0900

    Add the definition of `same_size_uint`

--- cutf/experimental/fp.hpp
+++ cutf/experimental/fp.hpp
@@ -2,10 +2,17 @@
 #define __CUTF_EXPERIMENTAL_FP_HPP__
 #include "../macro.hpp"
 #include <cuda_fp16.h>
+#include <cstdint>
 
 namespace cutf {
 namespace experimental {
 namespace fp {
+template <class T>
+struct same_size_uint {using type = void;};
+template <> struct same_size_uint<half  > {using type = uint16_t;};
+template <> struct same_size_uint<float > {using type = uint32_t;};
+template <> struct same_size_uint<double> {using type = uint64_t;};
+
 template <class T>
 CUTF_DEVICE_HOST_FUNC inline unsigned get_exponent_size();
 template <> CUTF_DEVICE_HOST_FUNC inline unsigned get_exponent_size<half  >() {return 5;}
