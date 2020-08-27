a9a772f0a64cfc596c3c448b5c629e5ab9f8c133
mutsuki <mutsuki@momo86.net>
2020-08-27 22:21:29 +0900

    Add experimental/fp.hpp

---
+++ cutf/experimental/fp.hpp
@@ -0,0 +1,10 @@
+#ifndef __CUTF_EXPERIMENTAL_FP_HPP__
+#define __CUTF_EXPERIMENTAL_FP_HPP__
+#include "../macro.hpp"
+
+namespace cutf {
+namespace experimental {
+
+} // namespace experimental
+} // namespace cutf
+#endif
