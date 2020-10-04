297cfb22a82ab9127a639c285968a9b4e2b9319b
mutsuki <mutsuki@momo86.net>
2020-10-04 17:28:34 +0900

    Add `threads::get_warp_id`

--- cutf/threads.hpp
+++ cutf/threads.hpp
@@ -4,6 +4,12 @@
 namespace cutf {
 namespace threads {
 __device__ inline unsigned get_lane_id() {
+	unsigned lane_id;
+	asm(R"({mov.s32 %0, %laneid;})":"=r"(lane_id));
+	return lane_id;
+}
+
+__device__ inline unsigned get_warp_id() {
 	unsigned warp_id;
 	asm(R"({mov.s32 %0, %warpid;})":"=r"(warp_id));
 	return warp_id;
