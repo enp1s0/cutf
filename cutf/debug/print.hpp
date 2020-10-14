77a39cd33ba866154e41b735296e4f39b690054c
mutsuki <mutsuki@momo86.net>
2020-10-14 19:06:30 +0900

    Update print.hpp to isolate `bitstring_t`

--- cutf/debug/print.hpp
+++ cutf/debug/print.hpp
@@ -2,27 +2,19 @@
 #define __CUTF_DEBUG_PRINT_HPP__
 #include <stdio.h>
 #include <cuda_fp16.h>
+#include "fp.hpp"
 namespace cutf {
 namespace debug {
 namespace print {
-template <class T>
-struct bitstring_t {using type = T;};
-template <> struct bitstring_t<half  > {using type = uint16_t;};
-template <> struct bitstring_t<float > {using type = uint32_t;};
-template <> struct bitstring_t<double> {using type = uint64_t;};
-template <> struct bitstring_t<const double> {using type = uint64_t;};
-template <> struct bitstring_t<const float > {using type = uint32_t;};
-template <> struct bitstring_t<const half  > {using type = uint64_t;};
-
 template <class T>
 __device__ __host__ inline void print_hex(const T v, const bool line_break = true);
 template <> __device__ __host__ inline void print_hex<uint64_t>(const uint64_t v, const bool line_break) {printf("0x%016lx", v);if(line_break)printf("\n");}
 template <> __device__ __host__ inline void print_hex<uint32_t>(const uint32_t v, const bool line_break) {printf("0x%08x", v);if(line_break)printf("\n");}
 template <> __device__ __host__ inline void print_hex<uint16_t>(const uint16_t v, const bool line_break) {printf("0x%04x", v);if(line_break)printf("\n");}
 template <> __device__ __host__ inline void print_hex<uint8_t >(const uint8_t  v, const bool line_break) {printf("0x%02x", v);if(line_break)printf("\n");}
-template <> __device__ __host__ inline void print_hex<double  >(const double   v, const bool line_break) {print_hex(*reinterpret_cast<const typename bitstring_t<double>::type*>(&v), line_break);}
-template <> __device__ __host__ inline void print_hex<float   >(const float    v, const bool line_break) {print_hex(*reinterpret_cast<const typename bitstring_t<float >::type*>(&v), line_break);}
-template <> __device__ __host__ inline void print_hex<half    >(const half     v, const bool line_break) {print_hex(*reinterpret_cast<const typename bitstring_t<half  >::type*>(&v), line_break);}
+template <> __device__ __host__ inline void print_hex<double  >(const double   v, const bool line_break) {print_hex(*reinterpret_cast<const typename cutf::debug::fp::bitstring_t<double>::type*>(&v), line_break);}
+template <> __device__ __host__ inline void print_hex<float   >(const float    v, const bool line_break) {print_hex(*reinterpret_cast<const typename cutf::debug::fp::bitstring_t<float >::type*>(&v), line_break);}
+template <> __device__ __host__ inline void print_hex<half    >(const half     v, const bool line_break) {print_hex(*reinterpret_cast<const typename cutf::debug::fp::bitstring_t<half  >::type*>(&v), line_break);}
 
 template <class T>
 __device__ __host__ inline void print_bin(const T v, const bool line_break = true) {
@@ -33,9 +25,9 @@ __device__ __host__ inline void print_bin(const T v, const bool line_break = tru
 		printf("\n");
 	}
 }
-template <> __device__ __host__ inline void print_bin<half  >(const half   v, const bool line_break) {print_bin(*reinterpret_cast<const typename bitstring_t<half  >::type*>(&v), line_break);}
-template <> __device__ __host__ inline void print_bin<float >(const float  v, const bool line_break) {print_bin(*reinterpret_cast<const typename bitstring_t<float >::type*>(&v), line_break);}
-template <> __device__ __host__ inline void print_bin<double>(const double v, const bool line_break) {print_bin(*reinterpret_cast<const typename bitstring_t<double>::type*>(&v), line_break);}
+template <> __device__ __host__ inline void print_bin<half  >(const half   v, const bool line_break) {print_bin(*reinterpret_cast<const typename cutf::debug::fp::bitstring_t<half  >::type*>(&v), line_break);}
+template <> __device__ __host__ inline void print_bin<float >(const float  v, const bool line_break) {print_bin(*reinterpret_cast<const typename cutf::debug::fp::bitstring_t<float >::type*>(&v), line_break);}
+template <> __device__ __host__ inline void print_bin<double>(const double v, const bool line_break) {print_bin(*reinterpret_cast<const typename cutf::debug::fp::bitstring_t<double>::type*>(&v), line_break);}
 } // namespace print
 } // namespace debug
 } // namespace cutf
