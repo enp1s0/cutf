2ea53f9d256dd92273662967c50ed93ee99b4dca
mutsuki <mutsuki@momo86.net>
2020-10-20 22:12:47 +0900

    Update `cutf::type::get_type_name` to support integer

--- cutf/type.hpp
+++ cutf/type.hpp
@@ -168,8 +168,15 @@ template <class T>
 CUTF_DEVICE_HOST_FUNC inline const char* get_type_name();
 template <> CUTF_DEVICE_HOST_FUNC inline const char* get_type_name<double >() {return "double";}
 template <> CUTF_DEVICE_HOST_FUNC inline const char* get_type_name<float  >() {return "float";}
-template <> CUTF_DEVICE_HOST_FUNC inline const char* get_type_name<__half >() {return "half";}
+template <> CUTF_DEVICE_HOST_FUNC inline const char* get_type_name<__half >() {return "half/(u)int16_t";}
 template <> CUTF_DEVICE_HOST_FUNC inline const char* get_type_name<__half2>() {return "half2";}
+template <> CUTF_DEVICE_HOST_FUNC inline const char* get_type_name<long long>() {return "uint64_t";}
+template <> CUTF_DEVICE_HOST_FUNC inline const char* get_type_name<uint64_t>() {return "uint64_t";}
+template <> CUTF_DEVICE_HOST_FUNC inline const char* get_type_name<uint32_t>() {return "uint32_t";}
+template <> CUTF_DEVICE_HOST_FUNC inline const char* get_type_name<uint16_t>() {return "uint8_t";}
+template <> CUTF_DEVICE_HOST_FUNC inline const char* get_type_name<int64_t>() {return "int64_t";}
+template <> CUTF_DEVICE_HOST_FUNC inline const char* get_type_name<int32_t>() {return "int32_t";}
+template <> CUTF_DEVICE_HOST_FUNC inline const char* get_type_name<int16_t>() {return "int8_t";}
 
 } // namespace type	
 } // cutf
