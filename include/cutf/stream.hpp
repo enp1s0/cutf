7eecb339438523a7a3f767e2d4e9d4ab232180e1
mutsuki <mutsuki@momo86.net>
2021-02-10 23:39:54 +0900

    Remove `device_id` from `get_stream_unique_ptr`

--- include/cutf/stream.hpp
+++ include/cutf/stream.hpp
@@ -15,11 +15,9 @@ struct stream_deleter{
 
 using stream_unique_ptr = std::unique_ptr<cudaStream_t, stream_deleter>;
 
-inline stream_unique_ptr get_stream_unique_ptr(const int device_id = 0){
-	cutf::error::check(cudaSetDevice(device_id), __FILE__, __LINE__, __func__, "@ Creating stream for device " + std::to_string(device_id));
+inline stream_unique_ptr get_stream_unique_ptr(){
 	stream_unique_ptr stream(new cudaStream_t);
-	cutf::error::check(cudaStreamCreate(stream.get()), __FILE__, __LINE__, __func__, "@ Creating stream for device " + std::to_string(device_id));
-	cutf::error::check(cudaSetDevice(0), __FILE__, __LINE__, __func__, "@ Creating stream for device " + std::to_string(device_id));
+	cutf::error::check(cudaStreamCreate(stream.get()), __FILE__, __LINE__, __func__, "@ Creating stream");
 	return stream;
 }
 } // stream
