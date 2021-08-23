e2e32f3037deeaa3bd69601ad53275925075169d
mutsuki <mutsuki@momo86.net>
2021-08-23 10:49:39 +0900

    Add debug/time_breakdown.hpp

---
+++ include/cutf/debug/time_breakdown.hpp
@@ -0,0 +1,13 @@
+#ifndef __CUTF_TIME_BREAKDOWN_HPP__
+#define __CUTF_TIME_BREAKDOWN_HPP__
+#include <vector>
+#include <chrono>
+namespace cutf {
+namespace debug {
+namespace time_breakdown {
+class profiler {
+};
+} // namespace time_breakdown
+} // namespace debug
+} // namespace cutf
+#endif
