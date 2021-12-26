b0dd56a0ec9af6c751b5127279c84fc31d098f90
mutsuki <mutsuki@momo86.net>
2021-12-26 19:19:30 +0900

    Update print matrix tests

--- samples/debug/print_matrix.cu
+++ samples/debug/print_matrix.cu
@@ -2,7 +2,7 @@
 #include <cutf/memory.hpp>
 #include <cutf/debug/matrix.hpp>
 
-constexpr unsigned N = 10;
+constexpr unsigned N = 4;
 
 int main() {
 	float h_mat[N * N];
@@ -12,6 +12,8 @@ int main() {
 	std::printf("# print_matrix\n");
 	cutf::debug::print::print_matrix(h_mat, N, N);
 	cutf::debug::print::print_numpy_matrix(h_mat, N, N);
+	cutf::debug::print::print_matrix(h_mat, N, N, N);
+	cutf::debug::print::print_numpy_matrix(h_mat, N, N, N);
 
 	auto d_uptr = cutf::memory::get_device_unique_ptr<float>(N * N);
 	cutf::memory::copy(d_uptr.get(), h_mat, N * N);
@@ -19,4 +21,6 @@ int main() {
 	std::printf("# print_matrix_from_host\n");
 	cutf::debug::print::print_matrix_from_host(d_uptr.get(), N, N);
 	cutf::debug::print::print_numpy_matrix_from_host(d_uptr.get(), N, N);
+	cutf::debug::print::print_matrix_from_host(d_uptr.get(), N, N, N);
+	cutf::debug::print::print_numpy_matrix_from_host(d_uptr.get(), N, N, N);
 }
