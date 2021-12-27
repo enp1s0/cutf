a99c8488f7da24e919e9e54a880a5bdc89dbb6dd
mutsuki <mutsuki@momo86.net>
2021-12-27 14:03:46 +0900

    Add time breakdown test

---
+++ samples/debug/time_breakdown.cu
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cutf/debug/time_breakdown.hpp>
+
+constexpr unsigned N = 1u << 20;
+constexpr unsigned block_size = 1u << 8;
+
+__global__ void plus_one_kernel(
+		float* const ptr,
+		const unsigned N
+		) {
+	const auto tid = blockDim.x * blockIdx.x + threadIdx.x;
+	if (tid >= N) {
+		return;
+	}
+
+	ptr[tid] += 1.f;
+}
+
+int main() {
+	cutf::debug::time_breakdown::profiler profiler;
+
+	float *da;
+	profiler.start_timer_sync("cudaMalloc");
+	cudaMalloc(reinterpret_cast<float**>(&da), sizeof(float));
+	profiler.stop_timer_sync("cudaMalloc");
+
+	profiler.start_timer_sync("kernel");
+	plus_one_kernel<<<(N + block_size - 1) / block_size, block_size>>>(da, N);
+	profiler.stop_timer_sync("kernel");
+
+	profiler.print_result(stdout);
+}
