f26de92d3f50c0237a87b126fff34c29bb19128c
mutsuki <mutsuki@momo86.net>
2023-01-15 18:04:00 +0900

    Add test for `print_matrix_hex`

--- examples/debug/print_matrix.cu
+++ examples/debug/print_matrix.cu
@@ -11,8 +11,10 @@ int main() {
 	}
 	std::printf("# print_matrix\n");
 	cutf::debug::print::print_matrix(h_mat, N, N);
+	cutf::debug::print::print_matrix_hex(h_mat, N, N);
 	cutf::debug::print::print_numpy_matrix(h_mat, N, N);
 	cutf::debug::print::print_matrix(h_mat, N, N, N);
+	cutf::debug::print::print_matrix_hex(h_mat, N, N, N);
 	cutf::debug::print::print_numpy_matrix(h_mat, N, N, N);
 
 	auto d_uptr = cutf::memory::get_device_unique_ptr<float>(N * N);
@@ -20,7 +22,9 @@ int main() {
 
 	std::printf("# print_matrix_from_host\n");
 	cutf::debug::print::print_matrix_from_host(d_uptr.get(), N, N);
+	cutf::debug::print::print_matrix_hex_from_host(d_uptr.get(), N, N);
 	cutf::debug::print::print_numpy_matrix_from_host(d_uptr.get(), N, N);
 	cutf::debug::print::print_matrix_from_host(d_uptr.get(), N, N, N);
+	cutf::debug::print::print_matrix_hex_from_host(d_uptr.get(), N, N, N);
 	cutf::debug::print::print_numpy_matrix_from_host(d_uptr.get(), N, N, N);
 }
