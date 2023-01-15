758b212fca724d439b87012bccd2df871a037cea
mutsuki <mutsuki@momo86.net>
2023-01-15 18:03:38 +0900

    Add `print_matrix_hex_from_host` and `print_matrix_hex`

--- include/cutf/debug/matrix.hpp
+++ include/cutf/debug/matrix.hpp
@@ -5,6 +5,7 @@
 #include "../type.hpp"
 #include "../macro.hpp"
 #include "../memory.hpp"
+#include "fp.hpp"
 
 namespace cutf {
 namespace debug {
@@ -25,6 +26,24 @@ CUTF_DEVICE_HOST_FUNC inline void print_matrix(const T* const ptr, const std::si
 template <class T>
 CUTF_DEVICE_HOST_FUNC inline void print_matrix(const T* const ptr, const std::size_t m, const std::size_t n, const char* const name = nullptr) {
 	print_matrix(ptr, m, n, m, name);
+}
+
+template <class T>
+CUTF_DEVICE_HOST_FUNC inline void print_matrix_hex(const T* const ptr, const std::size_t m, const std::size_t n, const std::size_t ldm, const char* const name = nullptr) {
+	if(name != nullptr) printf("%s = \n", name);
+	for(std::size_t i = 0; i < m; i++) {
+		for(std::size_t j = 0; j < n; j++) {
+			const auto val = cutf::type::cast<float>(ptr[j * ldm + i]);
+			print::print_hex(val, false);
+			printf(" ");
+		}
+		printf("\n");
+	}
+}
+
+template <class T>
+CUTF_DEVICE_HOST_FUNC inline void print_matrix_hex(const T* const ptr, const std::size_t m, const std::size_t n, const char* const name = nullptr) {
+	print_matrix_hex(ptr, m, n, m, name);
 }
 
 template <class T>
@@ -64,6 +83,24 @@ inline void print_matrix_from_host(const T* const ptr, const std::size_t m, cons
 	cutf::memory::copy(host_uptr.get(), ptr, m * n);
 
 	print_matrix(host_uptr.get(), m, n, name);
+}
+
+template <class T>
+inline void print_matrix_hex_from_host(const T* const ptr, const std::size_t m, const std::size_t n, const std::size_t ldm, const char* const name = nullptr) {
+	CUTF_CHECK_ERROR(cudaDeviceSynchronize());
+	auto host_uptr = cutf::memory::get_host_unique_ptr<T>(ldm * n);
+	cutf::memory::copy(host_uptr.get(), ptr, ldm * n);
+
+	print_matrix_hex(host_uptr.get(), m, n, ldm, name);
+}
+
+template <class T>
+inline void print_matrix_hex_from_host(const T* const ptr, const std::size_t m, const std::size_t n, const char* const name = nullptr) {
+	CUTF_CHECK_ERROR(cudaDeviceSynchronize());
+	auto host_uptr = cutf::memory::get_host_unique_ptr<T>(m * n);
+	cutf::memory::copy(host_uptr.get(), ptr, m * n);
+
+	print_matrix_hex(host_uptr.get(), m, n, name);
 }
 
 template <class T>
