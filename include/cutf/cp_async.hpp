a2a2360893dd131f176c4abdf03b8d349e70ee16
mutsuki <mutsuki@momo86.net>
2023-01-20 15:07:59 +0900

    Update cp_async

--- include/cutf/cp_async.hpp
+++ include/cutf/cp_async.hpp
@@ -1,44 +1,54 @@
 #ifndef __CUTF_CP_ASYNC_HPP__
 #define __CUTF_CP_ASYNC_HPP__
 #include <cstdint>
+#include "macro.hpp"
 namespace cutf {
 namespace cp_async {
 namespace detail {
-__device__ inline uint32_t get_smem_ptr_uint(const void* const ptr) {
+CUTF_DEVICE_FUNC inline uint32_t get_smem_ptr_uint(const void* const ptr) {
   uint32_t smem_ptr;
   asm volatile("{.reg .u64 smem_ptr; cvta.to.shared.u64 smem_ptr, %1; cvt.u32.u64 %0, smem_ptr; }\n": "=r"(smem_ptr) : "l"(ptr));
   return smem_ptr;
 }
 } // namespace detail
 
+#if defined(CUTF_DISABLE_CP_ASYNC) || (defined(__CUDA_ARCH__) && __CUDA_ARCH__ < 800)
+#else
+#define CUTF_INTERNAL_USE_CP_ASYNC
+#endif
+
 template <unsigned Size>
-__device__ inline void cp_async(void* const smem, const void* const gmem) {
-#if __CUDA_ARCH__ >= 800
+CUTF_DEVICE_FUNC inline void cp_async(void* const smem, const void* const gmem) {
 	static_assert(Size == 4 || Size == 8 || Size == 16, "Size must be one of 4, 8 and 16");
+#ifdef CUTF_INTERNAL_USE_CP_ASYNC
 	const unsigned smem_int_ptr = detail::get_smem_ptr_uint(smem);
 	asm volatile("{cp.async.ca.shared.global [%0], [%1], %2;}" :: "r"(smem_int_ptr), "l"(gmem), "n"(Size));
 #else
-	for (unsigned i = 0; i < Size / 4; i++) {
-		*(reinterpret_cast<uint32_t*>(smem) + i) = *(reinterpret_cast<const uint32_t*>(gmem) + i);
+	if (Size == 4) {
+		*(reinterpret_cast<uint32_t*>(smem)) = *(reinterpret_cast<const uint32_t*>(gmem));
+	} else if (Size == 8) {
+		*(reinterpret_cast<uint64_t*>(smem)) = *(reinterpret_cast<const uint64_t*>(gmem));
+	} else {
+		*(reinterpret_cast<ulong2*>(smem)) = *(reinterpret_cast<const ulong2*>(gmem));
 	}
 #endif
 }
 
-__device__ inline void commit() {
-#if __CUDA_ARCH__ >= 800
+CUTF_DEVICE_FUNC inline void commit() {
+#ifdef CUTF_INTERNAL_USE_CP_ASYNC
 	asm volatile("{cp.async.commit_group;}\n");
 #endif
 }
 
-__device__ inline void wait_all() {
-#if __CUDA_ARCH__ >= 800
+CUTF_DEVICE_FUNC inline void wait_all() {
+#ifdef CUTF_INTERNAL_USE_CP_ASYNC
 	asm volatile("{cp.async.wait_all;}");
 #endif
 }
 
 template <int N>
-__device__ inline void wait_group() {
-#if __CUDA_ARCH__ >= 800
+CUTF_DEVICE_FUNC inline void wait_group() {
+#ifdef CUTF_INTERNAL_USE_CP_ASYNC
 	asm volatile("{cp.async.wait_group %0;}":: "n"(N));
 #endif
 }
--- include/cutf/macro.hpp
+++ include/cutf/macro.hpp
@@ -1,13 +1,13 @@
 #ifndef __CUTF_MACRO_HPP__
 #define __CUTF_MACRO_HPP__
 
-#if !defined(CUTF_DEVICE_HOST_FUNC) && defined(__CUDA_ARCH__)
+#if !defined(CUTF_DEVICE_HOST_FUNC)
 #define CUTF_DEVICE_HOST_FUNC __device__ __host__
 #elif !defined(CUTF_DEVICE_HOST_FUNC)
 #define CUTF_DEVICE_HOST_FUNC
 #endif
 
-#if !defined(CUTF_DEVICE_FUNC) && defined(__CUDA_ARCH__)
+#if !defined(CUTF_DEVICE_FUNC)
 #define CUTF_DEVICE_FUNC __device__
 #elif !defined(CUTF_DEVICE_FUNC)
 #define CUTF_DEVICE_FUNC
