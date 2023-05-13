d498fcf314ba6449f75762064f3299429934bb87
mutsuki <mutsuki@momo86.net>
2023-05-13 16:17:58 +0900

    Fix NVTX test

--- examples/nvtx/nvtx.cu
+++ examples/nvtx/nvtx.cu
@@ -7,7 +7,7 @@ int main() {
 	cutf::nvtx::range_push("TEST_RANGE_1");
 	{
 		auto d_mem = cutf::memory::get_device_unique_ptr<double>(N);
-		cutf::nvtx::range_push("TEST_RANGE_1");
+		cutf::nvtx::range_push("TEST_RANGE_2");
 		CUTF_CHECK_ERROR(cudaMemset(d_mem.get(), 0xff, sizeof(double) * N));
 		cutf::nvtx::range_pop();
 	}
